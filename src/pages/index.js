61a255c8613a69c5418c6b51ada4f1488bc4e6c3
Quang Huy <quanghuy.h20@gmail.com>
2021-06-20 16:27:45 +0700

    Update components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,23 @@
 import * as React from "react"
 import PropTypes from 'prop-types';
-import { Layout } from '@components';
+import { Layout, Hero, About, Education, Projects, Contact } from '@components';
+import styled from 'styled-components'
 
-
+const StyledMain = styled.main`
+  counter-reset: section;
+`
 
 const IndexPage = ({ location }) => (
   <Layout location={location}>
-    
+    <StyledMain className="fill">
+      <Hero />
+      <About />
+      <Education />
+      <Projects />
+      <Contact />
+    </StyledMain>
   </Layout>
 );
-
 
 IndexPage.propTypes = {
   location: PropTypes.object.isRequired,
