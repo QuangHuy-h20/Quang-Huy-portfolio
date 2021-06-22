5c652c6031f5c1982f2aec6a27f904e37b6fff31
Quang Huy <quanghuy.h20@gmail.com>
2021-06-22 20:21:24 +0700

    Update a few of icons

--- src/components/icons/index.js
+++ src/components/icons/index.js
@@ -7,5 +7,6 @@ export { default as Codepen } from './codepen';
 export { default as Instagram } from './instagram';
 export { default as EducationIcon } from './education';
 export { default as Calendar } from './calendar';
+export { default as Code } from './code';
 export { default as External } from './external';
 export { default as Icon } from './icons';
